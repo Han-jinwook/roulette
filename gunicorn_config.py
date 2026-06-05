3ad3e32876638280038122d994f90b4e6cc7e845
Han-jinwook <chiu3@naver.com>
2026-06-05 13:26:47 +0900

    충돌 방지를 위해 eventlet 제거 및 gthread 50 할당으로 우회 해결

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,8 @@ import os
 
 bind = "0.0.0.0:" + os.getenv("PORT", "10000")  # Render의 PORT 환경 변수 사용
 workers = 1  # WebSocket 지원을 위해 반드시 1로 설정
-worker_class = "eventlet"  # SocketIO 비동기 처리를 위해 eventlet 사용
+worker_class = "gthread"  # Supabase 등 비동기 라이브러리와 충돌 없는 스레드 모드
+threads = 50  # 동시 접속 50명까지 15초 렉(고갈) 방지
 timeout = 120
 keepalive = 5
 preload_app = True
